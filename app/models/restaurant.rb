25b17acd0471f2ace4b69b989b9068ac2fd83a94
Raghu Betina <raghu@firstdraft.com>
2017-01-05 01:06:54 +0000

    Indirect association between restaurants and tags

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,6 +9,10 @@ class Restaurant < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :tags,
+             :through => :descriptors,
+             :source => :tag
+
   # Validations
 
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,6 +6,10 @@ class Tag < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :restaurants,
+             :through => :descriptors,
+             :source => :restaurant
+
   # Validations
 
 end
